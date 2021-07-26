4eae4a20d61bcc0e27f895dab8a850a6845f5a19
Francisco Gonzalez <franja506@gmail.com>
2021-07-26 02:17:51 -0300

    Se agrega las importarciones y exportaciones de recursos

--- 02-intro-javascript/src/bases/06-deses-obj.js
+++ 02-intro-javascript/src/bases/06-deses-obj.js
@@ -1,1 +1,32 @@
 
+// Desestructuracion
+// Accion desestructurante 
+
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman',
+    rango: 'Soldado'
+};
+
+
+const { nombre, edad, clave } = persona;
+
+console.log(`${nombre} | ${edad} | ${clave}`);
+
+const retornaPersona = ({nombre, clave,edad, rango = 'Capitan'}) => {
+
+    return {
+        nombreClave: clave,
+        anios: edad,
+        latlng: {
+            lat: 14.87,
+            lng: -56
+        }
+    }
+};
+
+const {nombreClave, anios,latlng:{ lat, lng}} = retornaPersona(persona);
+
+console.log(nombreClave, anios);
+console.log(lat, lng);
---
+++ 02-intro-javascript/src/bases/07-nose.js
@@ -0,0 +1,16 @@
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+
+const [, ,p3] = personajes;
+
+console.log(p3);
+
+const retornaArreglo = () => (['ABC',123]);
+
+const [letras,numeros] = retornaArreglo();
+
+console.log(letras, numeros);
+
+
+const retornoEstado = (valor) => ([valor,()=>{console.log('Hola Mundo')}]);
+
+const arr = retornoEstado('Goku');
